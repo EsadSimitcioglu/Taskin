402c135c7911d6da2d36a2e31eee226955ee7d3f
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-26 21:21:27 +0300

    Simple swagger UI added. Needs to be updated with further details.

--- src/main/java/com/example/Taskin/Controller/AnswerController.java
+++ src/main/java/com/example/Taskin/Controller/AnswerController.java
@@ -4,6 +4,9 @@ package com.example.Taskin.Controller;
 import com.example.Taskin.Model.Answer;
 import com.example.Taskin.Model.Comment;
 import com.example.Taskin.Service.AnswerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,18 +18,48 @@ public class AnswerController {
     AnswerService answerService;
 
     @PostMapping("/{id}/comments")
+    @Operation (summary = "Post a comment",
+                description = "Save new comment's content to database")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "201",
+                            description = "Successful operation"),
+                @ApiResponse(responseCode = "401",
+                        description = "Unsuccessful operation")})
     public Comment postCommentToAnswer(@PathVariable Integer id, @RequestBody Comment comment){return answerService.saveNewCommentToAnswer(id,comment.getCommentText(),comment.getCommentWriter());}
 
+
+    @Operation (summary = "Update an answer",
+            description = "Update answer's content to new values")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202",
+                    description = "Successful operation"),
+            @ApiResponse(responseCode = "401",
+                    description = "Unsuccessful operation")})
     @PutMapping
     public void putAnswer(@RequestBody Answer answer){
         answerService.updateAnswer(answer);
     }
 
+
+    @Operation (summary = "Upvote an answer",
+            description = "Increase the positive vote count of an answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Successful operation"),
+            @ApiResponse(responseCode = "401",
+                    description = "Unsuccessful operation")})
     @PutMapping("/{id}/votes/increases")
     public void putIncreaseVoteToAnswer(@PathVariable("id") Integer id){
         answerService.addVoteToAnswer(id);
     }
 
+    @Operation (summary = "Down-vote an answer",
+            description = "Increase the positive vote count of an answer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Successful operation"),
+            @ApiResponse(responseCode = "401",
+                    description = "Unsuccessful operation")})
     @PutMapping("/{id}/votes/decreases")
     public void putDecreaseVoteToAnswer(@PathVariable("id") Integer id){
         answerService.deleteVoteToAnswer(id);
--- src/main/java/com/example/Taskin/TaskinApplication.java
+++ src/main/java/com/example/Taskin/TaskinApplication.java
@@ -1,11 +1,15 @@
 package com.example.Taskin;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition( info = @Info(version = "1.0",
+								 title = "Taskin Project",
+								description = "Asking and Replying All Questions"))
 public class TaskinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskinApplication.class, args);
 	}
