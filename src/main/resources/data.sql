f8275bb063c292e6d650effdaa7c716aad8517f5
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2022-01-01 16:23:14 +0300

    DB initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into Users(user_id,user_name, user_password)
+values(1,'Abdullah', 'basit_parola');
+
+insert into Users(user_id,user_name, user_password)
+values(2,'Esad', '123ozge123');
+
+insert into Users(user_id,user_name, user_password)
+values(3,'Emin', '123irem123');
+
+insert into Users(user_id,user_name, user_password)
+values(4,'Esma Hoca', '!€$m@+MéRæI_');
+
+insert into Question(question_id,  question_description, question_title, user_user_id)
+values (1, 'MapStruct' , 'Hocam merhaba, mapStruct nasil kullanacagiz? Tesekkurler', 2)
