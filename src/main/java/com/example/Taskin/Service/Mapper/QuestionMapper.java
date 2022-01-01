8ba9bd4f0f8a2c81356f80256f24fe87c626ee69
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2022-01-01 18:12:22 +0300

    dto backwards mappings

--- src/main/java/com/example/Taskin/Service/Mapper/QuestionMapper.java
+++ src/main/java/com/example/Taskin/Service/Mapper/QuestionMapper.java
@@ -8,16 +8,28 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface QuestionMapper {
-    QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
+	QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
 
-    @Mapping(source = "questionID", target = "id")
-    @Mapping(source = "questionTitle", target = "title")
-    @Mapping(source = "questionDescription", target = "description")
-    @Mapping(source = "user", target = "author")
-    @Mapping(source = "date", target = "date")
-    @Mapping(source = "questionAnswerCount", target = "answerCount")
-    @Mapping(source = "questionViewCount", target = "viewCount")
-    @Mapping(source = "questionVoteCount", target = "voteCount")
-    @Mapping(source = "questionTags", target = "questionTags")
-    QuestionDTO questionToQuestionDTO(Question question);
+	@Mapping(source = "questionID", target = "id")
+	@Mapping(source = "questionTitle", target = "title")
+	@Mapping(source = "questionDescription", target = "description")
+	@Mapping(source = "user", target = "author")
+	@Mapping(source = "date", target = "date")
+	@Mapping(source = "questionAnswerCount", target = "answerCount")
+	@Mapping(source = "questionViewCount", target = "viewCount")
+	@Mapping(source = "questionVoteCount", target = "voteCount")
+	@Mapping(source = "questionTags", target = "questionTags")
+	QuestionDTO questionToQuestionDTO(Question question);
+
+	@Mapping(source = "id", target = "questionID")
+	@Mapping(source = "title", target = "questionTitle")
+	@Mapping(source = "description", target = "questionDescription")
+	@Mapping(source = "author", target = "user")
+	@Mapping(source = "date", target = "date")
+	@Mapping(source = "answerCount", target = "questionAnswerCount")
+	@Mapping(source = "viewCount", target = "questionViewCount")
+	@Mapping(source = "voteCount", target = "questionVoteCount")
+	@Mapping(source = "questionTags", target = "questionTags")
+	Question questionDTOToQuestion(QuestionDTO questionDTO);
+
 }
